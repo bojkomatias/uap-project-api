a6084d5f7dee808ed62c6a3e539f60129a235300
bojkomatias <55543631+bojkomatias@users.noreply.github.com>
2021-09-02 12:06:50 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15747947f88a7a778e6387aed06a0991'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c7595009-1d86-4b24-9128-b422d3a4705b'
+};
