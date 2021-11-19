fb12e0468b6a08aab9dd30d0d639121b1ce2eb90
bojkomatias <55543631+bojkomatias@users.noreply.github.com>
2021-11-19 17:57:25 -0300

    no cors

--- config/server.js
+++ config/server.js
@@ -1,10 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  cors: {
-    enabled: true,
-    headers: "*",
-  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '15747947f88a7a778e6387aed06a0991'),
