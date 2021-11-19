66d5531f2b986cde7c57aefce36aeebc8676a02d
bojkomatias <55543631+bojkomatias@users.noreply.github.com>
2021-11-19 16:15:52 -0300

    Dist/

--- public/dist/_nuxt/e643206.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{461:function(e,n,t){"use strict";t.r(n);var r={data:function(){return{sape:"sape"}}},c=t(87),component=Object(c.a)(r,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v(e._s(e.sape))])}),[],!1,null,"9769a552",null);n.default=component.exports}}]);
