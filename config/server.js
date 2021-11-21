3a9406b0ba7d84f826a7c4cd033e50441b1c14e8
bojkomatias <55543631+bojkomatias@users.noreply.github.com>
2021-11-21 20:41:51 -0300

    new config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '15747947f88a7a778e6387aed06a0991'),
-    },
-  },
+  url: 'https://api.practicas-fcs.uap.edu.ar',
 });
