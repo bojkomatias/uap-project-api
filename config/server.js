cb893a9336c41853df921eb7c477d12eb5f84600
bojkomatias <55543631+bojkomatias@users.noreply.github.com>
2021-11-21 21:21:50 -0300

    sape

--- config/server.js
+++ config/server.js
@@ -1,5 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://practicas-fcs.uap.edu.ar/api',
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15747947f88a7a778e6387aed06a0991'),
+    },
+  },
+  url: 'http://localhost:1337/api',
 });
